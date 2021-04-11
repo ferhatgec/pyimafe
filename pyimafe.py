beb4d3e21b0047fc2505fd9607b91898ae901e8b
ferhatgec <ferhat2gecdogan_fix_me5@protonmail.com>
2021-04-11 19:43:32 +0300

    Add: StackSwitcher & Stack & Width-Height

--- pyimafe.py
+++ pyimafe.py
@@ -25,10 +25,27 @@ class Imafe:
     title = 'PyImafe'
 
     header_bar = Gtk.HeaderBar()
+    grid = Gtk.Grid()
+
+    image_label = Gtk.Label()
+
+    filename = Gtk.Label()
+    resolution = Gtk.Label()
+
+    switcher = Gtk.StackSwitcher()
+    image_stack = Gtk.Stack()
+
+    info_box = Gtk.Box()
+
+    width = ''
+    height = ''
 
     def __init__(self):
         window = Gtk.Window()
         box = Gtk.Box()
+        width = ''
+        height = ''
+
 
         self.header_bar.set_title(self.title)
         self.header_bar.set_show_close_button(True)
@@ -36,11 +53,27 @@ class Imafe:
         window.set_size_request(600, 400)
         window.connect_after('destroy', self.destroy)
 
+        self.image_stack.set_vexpand(True)
+        self.image_stack.set_hexpand(True)
+
+        self.switcher.set_stack(self.image_stack)
+
         box.set_spacing(5)
 
         box.set_orientation(Gtk.Orientation.VERTICAL)
 
+        window.add(self.grid)
+
+        self.grid.attach(self.switcher, 0, 0, 1, 1)
+        self.grid.attach(self.image_stack, 0, 1, 1, 1)
+
+        self.image_stack.add_titled(box, 'Image', 'Image')
+        self.image_stack.add_titled(self.info_box, 'Image', 'Info')
+
         window.set_titlebar(self.header_bar)
+
+        self.info_box.pack_start(self.filename, True, True, 0)
+        self.info_box.pack_start(self.resolution, True, True, 1)
 
         window.add(box)
 
@@ -74,9 +107,16 @@ class Imafe:
         dialog.set_filter(filefilter)
 
         if dialog.run() == 1:
-            self.image.set_from_file(dialog.get_filename())
+            dialog_filename = dialog.get_filename()
 
-            self.header_bar.set_title(dialog.get_filename())
+            self.image.set_from_file(dialog_filename)
+
+            self.width = str(self.image.get_allocation().width)
+            self.height = str(self.image.get_allocation().height)
+
+            self.header_bar.set_title(dialog_filename)
+            self.filename.set_label(dialog_filename)
+            self.resolution.set_label(self.width + 'x' + self.height)
 
             dialog.destroy()
 
